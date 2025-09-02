cadccd51bc86b96da5aeefe0660f14a0161c9d2b
hiroraba <xornelius0313@gmail.com>
2025-09-03 08:56:28 +0900

    Initial Commit

---
+++ VolumeAdjuster/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  VolumeAdjuster
+//  
+//  Created by matsuohiroki on 2025/09/03.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VolumeAdjuster/VolumeAdjusterApp.swift
@@ -0,0 +1,18 @@
+//
+//  VolumeAdjusterApp.swift
+//  VolumeAdjuster
+//  
+//  Created by matsuohiroki on 2025/09/03.
+//  
+//
+
+import SwiftUI
+
+@main
+struct VolumeAdjusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
